c10974c06143693bc59aefb7ca23d2c419dd81a7
linmingwang <lin.mingwang@iwhalecloud.com>
2023-04-10 10:49:16 +0800

    first commit

---
+++ vite-vue3-ts-pinia/vite.config.ts
@@ -0,0 +1,37 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import * as path from 'path';
+import Components from 'unplugin-vue-components/vite';
+import { VantResolver } from 'unplugin-vue-components/resolvers';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    resolve: {
+        //设置别名
+        alias: {
+            '@': path.resolve(__dirname, 'src')
+        }
+    },
+    plugins: [
+        vue(),
+        Components({
+            resolvers: [VantResolver()]
+        })
+    ],
+    server: {
+        port: 8080, //启动端口
+        hmr: {
+            host: 'localhost',
+            port: 8080
+        },
+        open: true,
+        // 设置代理
+        proxy: {
+            '/api': {
+                target: 'your https address',
+                changeOrigin: true,
+                rewrite: (path: string) => path.replace(/^\/api/, '')
+            }
+        }
+    }
+});
